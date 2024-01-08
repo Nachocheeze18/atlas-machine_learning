c0ad4841ca17c99e520675c223bcedb5279a4e3a
Nachocheeze18 <5581@holbertonstudents.com>
2024-01-07 22:06:29 -0600

    documentation

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,6 +1,10 @@
+#!/user/bin/env python3
+"""Imports"""
 import gym
 
+
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """loads the pre-made FrozenLakeEnv evnironment from OpenAI’s gym"""
     if desc is not None or map_name is not None:
         env = gym.make('FrozenLake-v1', desc=desc, map_name=map_name, is_slippery=is_slippery)
     else:
