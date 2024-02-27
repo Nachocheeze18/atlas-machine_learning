48a46488f91c5c7caf85be3d548ba656e311a54b
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-27 17:43:57 -0600

    max temp

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- title and genre
-SELECT title, genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title ASC, genre_id ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
