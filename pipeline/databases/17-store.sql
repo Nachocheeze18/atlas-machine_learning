d97046d6920e4ec0bf32ffe9acfb9903116c9d8e
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 12:32:40 -0600

    create the trigger

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -3,9 +3,10 @@ DELIMITER //
 CREATE TRIGGER decrease_item_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
+    -- Decrease the quantity of the item in the items table
     UPDATE items
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE item_id = NEW.item_id;
+    SET quantity = quantity - NEW.quantity
+    WHERE item_name = NEW.item_name;
 END;
 //
 DELIMITER ;
