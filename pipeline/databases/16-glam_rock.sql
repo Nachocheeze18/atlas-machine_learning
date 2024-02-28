5eb6ff5734cbe29e483aa59739d5a1c8ab1e419e
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 11:13:41 -0600

    country orgins of bands

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,13 +1,11 @@
 -- lists all bands with Glam rock as their main style,
 SELECT 
     band_name,
-    CASE 
-        WHEN split IS NOT NULL THEN split - formed
-        ELSE 2020 - formed
-    END AS lifespan_until_2020
+    IFNULL((split-formed), (2020 - formed))
+    AS lifespan
 FROM 
     metal_bands
 WHERE 
     style LIKE '%Glam rock%'
 ORDER BY 
-    lifespan_until_2020 DESC;
+    lifespan DESC;
