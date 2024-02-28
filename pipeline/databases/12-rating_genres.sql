0beb3e870e85bb7f45613f650a5caeb1a75e4575
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 10:11:19 -0600

    rating shows

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,6 @@
+-- lists all genres by rating
+SELECT name, SUM(rate) AS rating FROM tv_genres 
+LEFT JOIN tv_show_genres on tv_genres.id=tv_show_genres.genre_id
+LEFT JOIN tv_show_ratings ON tv_show_genres.show_id=tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
