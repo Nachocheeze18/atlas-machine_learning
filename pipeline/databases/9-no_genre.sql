0199c7a4d3d2a5ba24fbf3178511f719e71b4ac6
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-27 18:07:23 -0600

    lists all shows

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows
 SELECT title, genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id
 WHERE genre_id IS NULL
 ORDER BY title ASC, genre_id ASC;
