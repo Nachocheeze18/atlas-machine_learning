32805dbfc76224b961fdf870595e558f7e1c7d82
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 13:33:54 -0600

    store average score

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,19 +1,19 @@
 -- store average score
-DELIMITER $$
+DELIMITER &&
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
 )
 BEGIN
-    DECLARE average_score DECIMAL(10, 1);
+    DECLARE average_score DEC(10, 1);
 
     SELECT AVG(score) INTO average_score
-    FROM corrections
-    WHERE user_id = user_id;
-
+        FROM corrections
+        WHERE user_id = corrections.user_id;
+    
     UPDATE users SET average_score = average_score WHERE id = user_id;
-END$$
+END; &&
 
 DELIMITER ;
