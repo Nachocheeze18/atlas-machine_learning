1397218d7fdbc70a0d13914259e7541e09cfae1b
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 13:05:51 -0600

    store average score

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,18 @@
+-- Create or replace the stored procedure
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+    INSERT INTO user_scores (user_id, average_score)
+    VALUES (user_id, avg_score)
+    ON DUPLICATE KEY UPDATE average_score = avg_score;
+
+END //
+
+DELIMITER ;
