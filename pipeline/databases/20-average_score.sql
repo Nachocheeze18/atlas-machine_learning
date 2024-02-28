9b0756a3fa619e23e13c81d921387b1368dac696
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 13:23:54 -0600

    store average score

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,29 +1,19 @@
 -- store average score
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser ()
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE user_id_val INT;
-    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    OPEN user_cursor;
-    user_loop: LOOP
-        FETCH user_cursor INTO user_id_val;
-        IF done THEN
-            LEAVE user_loop;
-        END IF;
-        DECLARE avg_score DECIMAL(10, 2);
-        SELECT AVG(score) INTO avg_score
-        FROM corrections
-        WHERE user_id = user_id_val;
-        INSERT INTO user_scores (user_id, average_score)
-        VALUES (user_id_val, avg_score)
-        ON DUPLICATE KEY UPDATE average_score = avg_score;
+    DECLARE average_score DECIMAL(10, 1);
 
-    END LOOP user_loop;
+    SELECT AVG(score) INTO average_score
+    FROM corrections
+    WHERE user_id = user_id;
 
-    CLOSE user_cursor;
-END //
+    UPDATE users SET average_score = average_score WHERE id = user_id;
+END$$
 
 DELIMITER ;
