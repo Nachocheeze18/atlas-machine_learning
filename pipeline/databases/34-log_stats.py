f2e9255477356084a7ace8676ea9b2bf0eb55e3e
Nachocheeze18 <5581@holbertonstudents.com>
2024-02-28 21:23:16 -0600

    log stats

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -4,16 +4,22 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client['logs']['nginx']
+    db = client['logs']
+    nginx_collection = db['nginx']
 
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
+    print("Methods:")
 
     http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
+    method_stats_dict = {}
+
     for method in http_methods:
         method_count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_count}")
+        method_stats_dict[method] = method_count
+
+    for method, count in method_stats_dict.items():
+        print(f"\tmethod {method}: {count}")
 
     status_check_count = nginx_collection.count_documents({'method': 'GET', 'path': "/status"})
     print(f"{status_check_count} status check")
